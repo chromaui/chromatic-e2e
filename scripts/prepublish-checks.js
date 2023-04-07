9b027d890e57e479d493f55a0b7c1d325611f804
Tom Coleman <tom@chromatic.com>
2023-04-07 21:32:32 +1000

    project setup

--- scripts/prepublish-checks.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable no-console */
-console.error(`
-  🙅 I am the gate keeper of this library. Delete the scripts folder once this project is ready to be released!
-`);
-process.exit(1);

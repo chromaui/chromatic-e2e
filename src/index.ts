9537955c2b0bbb86db9db252673b0a62c8937c9a
Tom Coleman <tom@chromatic.com>
2023-04-08 20:57:22 +1000

    Rename `takeSnapshot` to `takeArchive`

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ import { makeTest } from './playwright-api/makeTest';
 export const test = makeTest(base);
 export { expect };
 
-export { takeSnapshot as snapshot } from './playwright-api/takeSnapshot';
+export { takeSnapshot as takeArchive } from './playwright-api/takeSnapshot';

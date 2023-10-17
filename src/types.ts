03eecbf9cd73d7891303b82a16f270706b2964ce
Todd Evanoff <evanoff@gmail.com>
2023-10-17 15:42:43 -0700

    add comments about config options

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,23 @@
 export interface ChromaticConfig {
+  // https://www.chromatic.com/docs/delay/
   delay?: number;
+
+  // https://www.chromatic.com/docs/threshold/#anti-aliasing
   diffIncludeAntiAliasing?: boolean;
+
+  // https://www.chromatic.com/docs/threshold/#setting-the-threshold
   diffThreshold?: number;
+
+  // Disable the capture that happens automatically at the end of a test when using the Chromatic test fixture
   disableAutoCapture?: boolean;
+
+  // https://www.chromatic.com/docs/media-features/#test-high-contrast-color-schemes
   forcedColors?: string;
+
+  // https://www.chromatic.com/docs/animations/#css-animations
   pauseAnimationAtEnd?: boolean;
+
+  // https://www.chromatic.com/docs/media-features/#verify-reduced-motion-animations
   prefersReducedMotion?: string;
 }
 

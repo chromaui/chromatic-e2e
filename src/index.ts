019c4787164e5553e1cfb74d5054f9265e5cf825
Steven Kitterman <steven.kitterman@gmail.com>
2023-10-27 14:04:12 -0700

    Unwanted newline removed

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { test as base, expect } from '@playwright/test';
 import { makeTest } from './playwright-api/makeTest';
 
 export const test = makeTest(base);
-
 export { expect };
 
 export { takeArchive } from './playwright-api/takeArchive';

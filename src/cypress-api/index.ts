1b9b3db6a1c78589f662fb275b9b6bd2aee93dfe
Steven Kitterman <steven.kitterman@gmail.com>
2023-10-27 15:03:00 -0700

    Back to TS

--- src/cypress-api/index.ts
+++ src/cypress-api/index.ts
@@ -2,9 +2,13 @@ import { writeTestResult } from '../write-archive';
 import { SourceMapper } from '../utils/source-mapper';
 
 const doArchive = async ({
+  // @ts-ignore
   testTitle,
+  // @ts-ignore
   domSnapshots,
+  // @ts-ignore
   resourceArchive,
+  // @ts-ignore
   chromaticStorybookParams,
 }) => {
   let sourceMap = null;
@@ -19,19 +23,23 @@ const doArchive = async ({
     return [
       key,
       {
+        // @ts-ignore
         ...value,
         // we can't use Buffer in the browser (when we collect the responses)
         // so we go through one by one here and bufferize them
+        // @ts-ignore
         body: Buffer.from(value.body, 'utf8'),
       },
     ];
   });
 
   const allSnapshots = Object.fromEntries(
+    // @ts-ignore
     domSnapshots.map((item, index) => [`Snapshot #${index + 1}`, Buffer.from(JSON.stringify(item))])
   );
 
   await writeTestResult(
+    // @ts-ignore
     {
       title: testTitle,
       // doesn't matter what value we put here, as long as it's a subdirectory of where we want this to actually go
@@ -45,6 +53,7 @@ const doArchive = async ({
   );
 };
 
+// @ts-ignore
 export const archiveCypress = (stuff) => {
   doArchive(stuff);
 
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
-  entry: ['src/index.ts', 'src/cypress-api/index.js', 'src/cypress-api/support.js'],
+  entry: ['src/index.ts', 'src/cypress-api/index.ts', 'src/cypress-api/support.ts'],
   splitting: false,
   minify: !options.watch,
   format: ['cjs', 'esm'],

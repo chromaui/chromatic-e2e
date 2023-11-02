5e6ea97f05031e51fbdef9e232f74ba2e9b799e7
Todd Evanoff <evanoff@gmail.com>
2023-11-02 13:25:23 -0700

    consolidate all asset path testing into one page

--- __playwright-tests__/archiving-assets.spec.ts
+++ __playwright-tests__/archiving-assets.spec.ts
@@ -1,13 +1,5 @@
 import { test, expect } from '../src';
 
-test('file names are shortened to a valid length for the file system', async ({ page }) => {
-  await page.goto('/toolong');
-});
-
-test('file names will not collide with a directory of the same name', async ({ page }) => {
-  await page.goto('/conflict');
-});
-
-test('query strings are encoded into the file name', async ({ page }) => {
-  await page.goto('/assets-with-queryparams');
+test('asset paths', async ({ page }) => {
+  await page.goto('/asset-paths');
 });

8c7a16917268a9b2d52e391fd05a8b4d7a8e95d1
Steven Kitterman <steven.kitterman@gmail.com>
2023-11-06 12:27:09 -0800

    Cypress types added

--- src/cypress-api/commands.ts
+++ src/cypress-api/commands.ts
@@ -2,13 +2,12 @@ import { snapshot } from '@chromaui/rrweb-snapshot';
 import type { elementNode } from '@chromaui/rrweb-snapshot';
 // @ts-expect-error will fix when Cypress has its own package
 Cypress.Commands.add('takeChromaticArchive', () => {
-  // @ts-expect-error will fix when Cypress has its own package
   cy.document().then((doc) => {
     // here, handle the source map
     const manualSnapshot = snapshot(doc, { noAbsolute: true });
     // reassign manualSnapshots so it includes this new snapshot
-    // @ts-expect-error will fix when Cypress has its own package
     cy.get('@manualSnapshots')
+      // @ts-expect-error will fix when Cypress has its own package
       .then((snapshots: elementNode[]) => {
         return [...snapshots, manualSnapshot];
       })

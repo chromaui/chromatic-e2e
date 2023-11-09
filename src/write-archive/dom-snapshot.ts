6280f511e665f8f4cd6e1685866949244539593e
Todd Evanoff <evanoff@gmail.com>
2023-11-08 19:26:51 -0800

    map css urls in style elements

--- src/write-archive/dom-snapshot.ts
+++ src/write-archive/dom-snapshot.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-param-reassign */
 import type { serializedNodeWithId } from '@chromaui/rrweb-snapshot';
 import { NodeType } from '@chromaui/rrweb-snapshot';
@@ -27,6 +28,7 @@ export class DOMSnapshot {
 
   private async mapNode(node: serializedNodeWithId, sourceMap: Map<string, string>) {
     node = this.mapNodeAttributes(node, sourceMap);
+    node = this.mapTextElement(node, sourceMap);
 
     if ('childNodes' in node) {
       node.childNodes = await Promise.all(
@@ -53,6 +55,25 @@ export class DOMSnapshot {
         const mappedCssText = this.mapCssUrls(cssText, sourceMap);
         node.attributes.style = mappedCssText;
       }
+
+      // This is how rr-web stores the contents of an external stylesheet
+      // that it will put into a `style` tag on render
+      if (node.attributes._cssText) {
+        const cssText = node.attributes._cssText as string;
+        const mappedCssText = this.mapCssUrls(cssText, sourceMap);
+        node.attributes._cssText = mappedCssText;
+      }
+    }
+
+    return node;
+  }
+
+  private mapTextElement(node: serializedNodeWithId, sourceMap: Map<string, string>) {
+    if (node.type === NodeType.Text && node.isStyle) {
+      if (node.textContent) {
+        const mappedCssText = this.mapCssUrls(node.textContent, sourceMap);
+        node.textContent = mappedCssText;
+      }
     }
 
     return node;
@@ -68,4 +89,5 @@ export class DOMSnapshot {
     });
   }
 }
+/* eslint-enable no-underscore-dangle */
 /* eslint-enable no-param-reassign */

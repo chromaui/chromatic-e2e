71dfc1a237b18cbb7701a68efa54e5181fe62498
Steven Kitterman <steven.kitterman@gmail.com>
2023-12-20 15:05:10 -0800

    Cypress config put in subdirectory to make CLI happy

--- __tests__/cypress/e2e/spec.cy.ts
+++ __tests__/cypress/e2e/spec.cy.ts
@@ -1,7 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io');
-    // @ts-expect-error this "expect" is Cypress (Mocha), not Jest
-    expect(1).to.equal(1);
+    expect(1).to.equal(2);
   });
 });

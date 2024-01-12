1cecb665c487b0888dde27da6b799453d5f3d6cd
Todd Evanoff <evanoff@gmail.com>
2024-01-12 14:50:54 -0800

    fix addon path

--- packages/shared/storybook-config/main.ts
+++ packages/shared/storybook-config/main.ts
@@ -4,7 +4,7 @@ import { archivesDir } from '@chromaui/shared-e2e';
 /** @type { import('@storybook/server-webpack5').StorybookConfig } */
 export default {
   stories: [path.resolve(archivesDir(), '*.stories.json')],
-  addons: ['@storybook/addon-essentials', '../dist'],
+  addons: ['@storybook/addon-essentials', '.'],
   framework: {
     name: '@storybook/server-webpack5',
     options: {},

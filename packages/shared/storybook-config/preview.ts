df3485aa9a1b7e206503e13bb2661f8c951d66bb
Tom Coleman <tom@chromatic.com>
2024-01-13 08:59:01 +1100

    Switch to using a script tag and clean up a bit

--- packages/shared/storybook-config/preview.ts
+++ packages/shared/storybook-config/preview.ts
@@ -29,20 +29,31 @@ const renderToCanvas: RenderToCanvas<RRWebFramework> = async (context, element) 
   const response = await fetch(`${url}/${id}`);
   const snapshot = await response.json();
 
+  // The snapshot is a representation of a complete HTML document. The first child is the
+  // doc type declaration and the second is the html element.
   const htmlNode = snapshot.childNodes[1];
 
+  // If you rebuild the full snapshot with rrweb (the document) it will replace the
+  // current document and call `document.open()` in the process, which unbinds all event handlers
+  // (and breaks Storybook).
+  // However, if you just rebuild the html element part, it will recreate but not attempt to
+  // insert it in the DOM.
   // @ts-expect-error rebuild is typed incorreclty, cache and mirror are optional
   const html = (await rebuild(htmlNode, { doc: document })) as HTMLElement;
 
-  document.children[0].innerHTML = html.innerHTML;
+  // Now we insert the rebuilt html element in the DOM
+  document.replaceChild(html, document.children[0]);
 
-  // insert a couple of elements to fool SB
-  document.body.innerHTML += '<div id="storybook-root"></div><div id="storybook-docs"></div>';
+  // Storybook's WebView will throw an error if it cannot find these two ids in the DOM.
+  // We never render docs (so the #storybook-docs doesn't matter), and our`renderToCanvas`
+  // function is already ignoring the #storybook-root (`element` above), so it doesn't matter where
+  // they are or what they contain.
+  // We make them a script in the head to ensure they don't impact layout.
+  document.head.innerHTML +=
+    '<script id="storybook-root"></script><script id="storybook-docs"></script>';
 
   context.showMain();
-  return () => {
-    // element.removeChild(iframe);
-  };
+  return () => {}; // We can't really cleanup
 };
 
 export default {

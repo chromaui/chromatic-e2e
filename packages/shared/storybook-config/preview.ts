d69fea29e73f61ab13f194a451c1e32282b9cbeb
Todd Evanoff <evanoff@gmail.com>
2024-01-15 20:13:01 -0800

    remove unused code

--- packages/shared/storybook-config/preview.ts
+++ packages/shared/storybook-config/preview.ts
@@ -1,7 +1,5 @@
 import type { RenderToCanvas, WebRenderer } from '@storybook/types';
 import { rebuild } from 'rrweb-snapshot';
-// eslint-disable-next-line no-restricted-imports
-import debounce from 'lodash.debounce';
 
 const pageUrl = new URL(window.location.href);
 pageUrl.pathname = '';
@@ -11,17 +9,6 @@ const strippedUrl = pageUrl.toString().replace(/\/$/, '');
 export interface RRWebFramework extends WebRenderer {
   component: undefined;
   storyResult: Record<string, never>;
-}
-
-const iframeStyle = 'border: 0; min-width: 100vw; min-height: 100vh;';
-// Update the style attribute to set the width/height correctly
-function updateDimensions(iframe: HTMLIFrameElement) {
-  const { scrollWidth, scrollHeight } = iframe.contentDocument.body;
-  iframe.setAttribute(
-    'style',
-    `${iframeStyle}; width: ${scrollWidth}px; height: ${scrollHeight}px;`
-  );
-  iframe.setAttribute('id', 'chromatic-e2e-inner-iframe');
 }
 
 const renderToCanvas: RenderToCanvas<RRWebFramework> = async (context, element) => {

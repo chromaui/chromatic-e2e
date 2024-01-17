4f31fd70fba70d5afbf81d155c387f74fc41d7b5
Steven Kitterman <steven.kitterman@gmail.com>
2024-01-17 15:58:24 -0800

    Multiple snapshots taken

--- packages/playwright/tests/manual-snapshots.spec.ts
+++ packages/playwright/tests/manual-snapshots.spec.ts
@@ -1,7 +1,8 @@
-import { test, expect } from '../src';
+import { test, expect, takeSnapshot } from '../src';
 
-test('Manual snapshots / multiple snapshots are taken', async ({ page }) => {
+test('Manual snapshots / multiple snapshots are taken', async ({ page }, testInfo) => {
   await page.goto('/manual-snapshots');
+  await takeSnapshot(page, testInfo);
   await page.locator('summary').click();
   await expect(page.getByText('I am hiding inside!')).toBeVisible();
 });

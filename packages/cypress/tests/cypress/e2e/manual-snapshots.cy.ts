0909035cf3d4d557ab7dbf3b86b45a435db0f68b
Steven Kitterman <steven.kitterman@gmail.com>
2024-01-18 15:45:02 -0800

    Test shows manual snapshots both with and without custom names

--- packages/cypress/tests/cypress/e2e/manual-snapshots.cy.ts
+++ packages/cypress/tests/cypress/e2e/manual-snapshots.cy.ts
@@ -1,6 +1,10 @@
 it('Manual Snapshots / multiple snapshots are taken', () => {
   cy.visit('/manual-snapshots');
+  // manual snapshot with name
   cy.takeSnapshot('accordion collapsed');
   cy.contains("I'm an accordion, click me!").click();
-  cy.contains('I am hiding inside!').should('be.visible');
+  // manual snapshot without name
+  cy.takeSnapshot();
+  cy.contains("I'm an accordion, click me!").click();
+  cy.get('details').should('not.have.attr', 'open');
 });

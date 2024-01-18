d6ff9051daceca6e109c8344c901fbeed61a421a
Steven Kitterman <steven.kitterman@gmail.com>
2024-01-18 11:54:03 -0800

    Electron now works in interactive mode, without requiring users to pass the ELECTRON_EXTRA_LAUNCH_ARGS env variable

--- packages/cypress/src/index.ts
+++ packages/cypress/src/index.ts
@@ -127,8 +127,13 @@ export const onBeforeBrowserLaunch = (
   // we don't use the browser parameter but we're keeping it here in case we'd ever need to read from it
   // (this way users wouldn't have to change their cypress.config file as it's already passed to us)
   browser: Cypress.Browser,
-  launchOptions: Cypress.BeforeBrowserLaunchOptions
+  launchOptions: Cypress.BeforeBrowserLaunchOptions,
+  config: any
 ) => {
+  if (config.isInteractive) {
+    return;
+  }
+
   const hostArg = launchOptions.args.find((arg) => arg.startsWith('--remote-debugging-address='));
   host = hostArg ? hostArg.split('=')[1] : '127.0.0.1';
 
@@ -155,10 +160,13 @@ export const onBeforeBrowserLaunch = (
   return launchOptions;
 };
 
-export const installPlugin = (on: any) => {
+export const installPlugin = (on: any, config: any) => {
   // these events are run on the server (in Node)
   on('task', {
     prepareArchives,
   });
-  on('before:browser:launch', onBeforeBrowserLaunch);
+
+  on('before:browser:launch', (browser: any, launchOptions: any) => {
+    onBeforeBrowserLaunch(browser, launchOptions, config);
+  });
 };
--- packages/cypress/src/support.ts
+++ packages/cypress/src/support.ts
@@ -1,8 +1,15 @@
 import { snapshot } from 'rrweb-snapshot';
 import './commands';
 
+const shouldTakeSnapshot = () => {
+  return !Cypress.env('disableAutoSnapshot') && !Cypress.config('isInteractive');
+};
+
 // these client-side lifecycle hooks will be added to the user's Cypress suite
 beforeEach(() => {
+  if (!shouldTakeSnapshot()) {
+    return;
+  }
   // this "manualSnapshots" variable will be available before, during, and after the test,
   // then cleaned up before the next test is run
   // (see https://docs.cypress.io/guides/core-concepts/variables-and-aliases#Aliases)
@@ -14,7 +21,7 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-  if (Cypress.env('disableAutoSnapshot')) {
+  if (!shouldTakeSnapshot()) {
     return;
   }
   // can we be sure this always fires after all the requests are back?

0ee3ca18e7356ed901d6fd67079d3ac6414e533c
Steven Kitterman <steven.kitterman@gmail.com>
2024-01-17 16:48:10 -0800

    Cypress manual snapshots can be given a name

--- packages/cypress/tests/cypress/e2e/manual-snapshots.cy.ts
+++ packages/cypress/tests/cypress/e2e/manual-snapshots.cy.ts
@@ -1,6 +1,6 @@
 it('Manual Snapshots / multiple snapshots are taken', () => {
   cy.visit('/manual-snapshots');
-  cy.takeSnapshot();
+  cy.takeSnapshot('accordion collapsed');
   cy.contains("I'm an accordion, click me!").click();
   cy.contains('I am hiding inside!').should('be.visible');
 });

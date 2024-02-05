16bce0163555b042265bf1e6e971f93dfc68c92c
Steven Kitterman <steven.kitterman@gmail.com>
2024-02-05 12:53:48 -0800

    Better comment

--- packages/shared/storybook-config/manager.ts
+++ packages/shared/storybook-config/manager.ts
@@ -2,6 +2,7 @@ import { addons } from '@storybook/manager-api';
 
 addons.setConfig({
   sidebar: {
+    // this ensures we use folders at the root-level instead of categories
     showRoots: false,
   },
 });

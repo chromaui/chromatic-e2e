e0096133dd0d66015899303d77e4c7d49e67acae
Todd Evanoff <evanoff@gmail.com>
2024-02-20 15:06:16 -0800

    set cypress viewports

--- packages/cypress/tests/cypress/e2e/viewports.cy.ts
+++ packages/cypress/tests/cypress/e2e/viewports.cy.ts
@@ -1,3 +1,21 @@
-it('snapshots capture the correct viewport size', () => {
-  cy.visit('/viewports');
+describe('desktop', { viewportWidth: 1200, viewportHeight: 720 }, () => {
+  it('snapshots capture the correct viewport size', () => {
+    cy.visit('/viewports');
+  });
+});
+
+context('mobile', { viewportWidth: 500, viewportHeight: 500 }, () => {
+  it('snapshots capture the correct viewport size', () => {
+    cy.visit('/viewports');
+  });
+});
+
+context('hardcoded viewport', () => {
+  beforeEach(() => {
+    cy.viewport(800, 720);
+  });
+
+  it('does not display the large or small viewport copy', () => {
+    cy.visit('/viewports');
+  });
 });

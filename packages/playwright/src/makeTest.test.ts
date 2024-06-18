96857480e64a75a59f2a1744f68eb90d7257f2eb
Steven Kitterman <steven.kitterman@gmail.com>
2024-06-18 16:02:43 -0700

    Test ensures we remove records from snapshots

---
+++ packages/playwright/src/makeTest.test.ts
@@ -0,0 +1,38 @@
+import { TestInfo } from 'playwright/test';
+import { performChromaticSnapshot } from './makeTest';
+import { Browser, chromium, Page } from 'playwright';
+import { chromaticSnapshots } from './takeSnapshot';
+
+describe('makeTest', () => {
+  let browser: Browser;
+  let page: Page;
+
+  beforeEach(async () => {
+    // create a bare-bones Playwright test launch (https://playwright.dev/docs/library)
+    browser = await chromium.launch();
+    page = await browser.newPage();
+  });
+
+  afterEach(async () => {
+    await browser.close();
+  });
+
+  it('Removes the entry from the snapshots after test is done', async () => {
+    const mockTestInfo: Partial<TestInfo> = {
+      titlePath: [],
+      outputDir: '',
+    };
+
+    expect(chromaticSnapshots).toEqual({});
+    await performChromaticSnapshot(
+      { page, shouldTrack: false },
+      async () => {
+        // do-nothing test() function
+      },
+      // @ts-expect-error mockTestInfo doesn't try to have all the TestInfo fields. I didn't see a way of deriving testInfo
+      // from the page/browser itself (typically that comes as part of the Playwright test runner, which we're outside of here)
+      mockTestInfo
+    );
+    expect(chromaticSnapshots).toEqual({});
+  });
+});

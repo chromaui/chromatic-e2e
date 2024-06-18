d486a5d8410428561ce05fd888e2e76acbda82db
Steven Kitterman <steven.kitterman@gmail.com>
2024-06-18 16:46:23 -0700

    Silence test errors

--- packages/playwright/src/takeSnapshot.test.ts
+++ packages/playwright/src/takeSnapshot.test.ts
@@ -1,69 +1,19 @@
-import { dedent } from 'ts-dedent';
-import express, { type Request } from 'express';
-import { Server } from 'http';
-import { Browser, chromium, devices, Page } from 'playwright';
+import { Page } from 'playwright';
 
 import { chromaticSnapshots, takeSnapshot } from './takeSnapshot';
 import { TestInfo } from 'playwright/test';
 
-const TEST_PORT = 13339;
-
-const baseUrl = `http://localhost:${TEST_PORT}`;
-
-const indexHtml = dedent`
-  <html>
-    <head>
-      <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-      <img src="img.png" >
-    </body>
-  </html>
-`;
-
-const pathToResponseInfo = {
-  '/': {
-    content: ({ query: { inject = '' } }: Request) =>
-      indexHtml.replace('</body>', `${[].concat(inject).map(decodeURIComponent).join('')}</body>`),
-    mimeType: 'text/html',
-  },
-} as const;
-
-let app: ReturnType<typeof express>;
-let server: Server;
-beforeEach(async () => {
-  app = express();
-
-  Object.entries(pathToResponseInfo).forEach(([path, responseInfo]) => {
-    app.get(path, (req, res) => {
-      const { content, mimeType } = responseInfo;
-      res.header('content-type', mimeType);
-      res.send(typeof content === 'function' ? content(req) : content);
-    });
-  });
-
-  await new Promise((resolve) => {
-    server = app.listen(TEST_PORT, () => resolve(null));
-  });
-});
-
-afterEach(async () => {
-  await server.close();
-});
+// @ts-expect-error fakePage isn't a proper page, but we just need to fake enough to run the test
+// (where page is probably too tied up into takeSnapshot anyway)
+const fakePage = {
+  on: () => {},
+  // @ts-expect-error return type of evaluate isn't a concern to us here as we test
+  // in the Chromatic snapshots that the snapshot actually captures what we want
+  evaluate: () => null,
+} as Page;
 
 describe('Snapshot storage', () => {
-  let browser: Browser;
-  let page: Page;
-
   beforeEach(async () => {
-    // create a bare-bones Playwright test launch (https://playwright.dev/docs/library)
-    browser = await chromium.launch();
-    page = await browser.newPage();
-  });
-
-  afterEach(async () => {
-    await browser.close();
-
     // we have to manually clear out the chromaticSnapshots entries since that behavior only happens
     // in our test fixture (one level up).
     chromaticSnapshots.clear();
@@ -72,12 +22,10 @@ describe('Snapshot storage', () => {
   it('creates an entry (test name and snapshot buffer) when a snapshot is taken', async () => {
     expect(chromaticSnapshots.size).toBe(0);
 
-    await page.goto(baseUrl);
-
     // not ideal to mock testInfo, but AFAIK we can't get testInfo when using Playwright library instead of Playwright test runner.
     // and this way we can specify the test ID ourselves
     const fakeTestInfo = { testId: 'a' };
-    await takeSnapshot(page, fakeTestInfo as TestInfo);
+    await takeSnapshot(fakePage, fakeTestInfo as TestInfo);
 
     expect(chromaticSnapshots.get('a').has('Snapshot #1'));
     expect(Buffer.isBuffer(chromaticSnapshots.get('a').get('Snapshot #1'))).toBe(true);
@@ -86,12 +34,10 @@ describe('Snapshot storage', () => {
   it('creates multiple entries when multiple snapshots are taken', async () => {
     expect(chromaticSnapshots.size).toBe(0);
 
-    await page.goto(baseUrl);
-
     const fakeTestInfo = { testId: 'a' };
     // take multiple snapshots
-    await takeSnapshot(page, fakeTestInfo as TestInfo);
-    await takeSnapshot(page, fakeTestInfo as TestInfo);
+    await takeSnapshot(fakePage, fakeTestInfo as TestInfo);
+    await takeSnapshot(fakePage, fakeTestInfo as TestInfo);
 
     expect(chromaticSnapshots.get('a').has('Snapshot #1'));
     expect(chromaticSnapshots.get('a').has('Snapshot #2'));
@@ -101,8 +47,8 @@ describe('Snapshot storage', () => {
     expect(chromaticSnapshots.size).toBe(0);
 
     const fakeTestInfo = { testId: 'a' };
-    await takeSnapshot(page, 'first snappy', fakeTestInfo as TestInfo);
-    await takeSnapshot(page, 'second snappy', fakeTestInfo as TestInfo);
+    await takeSnapshot(fakePage, 'first snappy', fakeTestInfo as TestInfo);
+    await takeSnapshot(fakePage, 'second snappy', fakeTestInfo as TestInfo);
 
     expect(chromaticSnapshots.get('a').has('first snappy'));
     expect(chromaticSnapshots.get('a').has('second snappy'));

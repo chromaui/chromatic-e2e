27ce9f6b717251f841dcc00a1bd91da6e4902ebc
Todd Evanoff <evanoff@gmail.com>
2024-07-02 16:54:30 -0700

    add test verifying that the correct cypress dir is used

--- packages/cypress/tests/cypress.config.ts
+++ packages/cypress/tests/cypress.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'cypress';
 import { installPlugin } from '../dist';
+import { existsSync } from 'node:fs';
 
 export default defineConfig({
+  // `downloadsFolder` cannot be overridden in tests, so we're setting
+  // this to a non-default value for asserting in the tests
+  downloadsFolder: 'cypress/new-downloads',
   // needed since we use common mock images between Cypress and Playwright
   fixturesFolder: '../../../test-server/fixtures',
   screenshotOnRunFailure: false,
@@ -11,7 +15,7 @@ export default defineConfig({
       installPlugin(on, config);
       on('task', {
         directoryExists(directoryName) {
-          return true;
+          return existsSync(directoryName);
         },
       });
     },
--- packages/cypress/tests/cypress/e2e/custom-downloads-directory.cy.ts
+++ packages/cypress/tests/cypress/e2e/custom-downloads-directory.cy.ts
@@ -1,7 +1,9 @@
 context('using Cypress.config', () => {
   it('Downloads archives to the user-specified folder', () => {
     cy.visit('/viewports');
-    const dirExists = cy.task('directoryExists', '/some-dir');
-    console.log('DIRECTORY EXISTS', dirExists);
+    const chromaticArchivesDir = `${Cypress.config('downloadsFolder')}/chromatic-archives`;
+    cy.task('directoryExists', chromaticArchivesDir).then((dirExists) => {
+      expect(dirExists).true;
+    });
   });
 });

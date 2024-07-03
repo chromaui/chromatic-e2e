46b99269555db8bd109cc134853a27d0c61a049d
Steven Kitterman <steven.kitterman@gmail.com>
2024-07-03 14:28:13 -0700

    Responses decrement the number of in-flight requests

--- packages/cypress/src/network-idle-watcher.test.ts
+++ packages/cypress/src/network-idle-watcher.test.ts
@@ -16,10 +16,69 @@ it('Resolves when there is no network activity', async () => {
   await expect(watcher.idle()).resolves.toBeDefined();
 });
 
-it('Rejects when timeout is hit, if request never returns', async () => {
+it.skip('Rejects when timeout is hit, if request never returns', async () => {
   const watcher = new NetworkIdleWatcher();
   // fire off request
   watcher.onRequest();
   // no response fired off
   await expect(watcher.idle()).rejects.toBeDefined();
 });
+
+it('Resolves when there is a single request and response', async () => {
+  const watcher = new NetworkIdleWatcher();
+
+  watcher.onRequest();
+  watcher.onResponse();
+
+  await expect(watcher.idle()).resolves.toBeDefined();
+});
+
+it('Resolves when there are an equal amount of requests and responses', async () => {
+  const watcher = new NetworkIdleWatcher();
+
+  watcher.onRequest();
+  watcher.onResponse();
+
+  watcher.onRequest();
+  watcher.onResponse();
+
+  watcher.onRequest();
+  watcher.onRequest();
+
+  watcher.onResponse();
+  watcher.onResponse();
+
+  await expect(watcher.idle()).resolves.toBeDefined();
+});
+
+const waitForResponse = async (watcher, timeInMs) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      watcher.onResponse();
+      resolve();
+    }, timeInMs);
+  });
+};
+
+it.skip('Given multiple cascading requests, resolves when all requests are returned, even though collectively they take a long time', async () => {
+  const watcher = new NetworkIdleWatcher();
+  // Each individual request is smaller than the interval to consider the network "idle", but collectively longer than
+  // the watcher will wait for a single test.
+  watcher.onRequest();
+
+  await waitForResponse(watcher, 2000);
+
+  watcher.onRequest();
+
+  await waitForResponse(watcher, 2000);
+
+  watcher.onRequest();
+
+  await waitForResponse(watcher, 2000);
+
+  await expect(watcher.idle()).resolves.toBeDefined();
+});
+
+// make sure resolves/rejects hasn't been called until all requests are back (not resolving when any old thing returns)
+
+// then only do smaller interval if fewer than 2 resources waiting
--- packages/cypress/src/network-idle-watcher.ts
+++ packages/cypress/src/network-idle-watcher.ts
@@ -1,19 +1,22 @@
 export class NetworkIdleWatcher {
   private numInFlightRequests = 0;
+  // private idleTimer = null;
 
   async idle() {
     return new Promise((resolve, reject) => {
       if (this.numInFlightRequests === 0) {
         resolve('cool');
       } else {
-        setTimeout(() => {
-          reject('done');
-        }, 3000);
+        reject('done');
       }
     });
   }
 
   onRequest() {
     this.numInFlightRequests += 1;
+  }
+
+  onResponse() {
+    this.numInFlightRequests -= 1;
   }
 }

22df64d0f7232c63f00372f11477550a668017dd
Todd Evanoff <evanoff@gmail.com>
2024-07-08 12:59:56 -0700

    add tests for long test/story names
    These will break the test until a fix is in place.

---
+++ packages/cypress/tests/cypress/e2e/long-test-names.cy.ts
@@ -0,0 +1,5 @@
+describe('this is a very long story name it just keeps going and going and it cannot stop and it will not stop ba bada da da dum dum dum', () => {
+  it('and this is also an incredibly long test name because there are just a bunch of random chars at the end like this ldlk elke lekj felk felkf lkf lsf lkef lse flskef ls fls eflsj flksef', () => {
+    cy.visit('/');
+  });
+});
---
+++ packages/playwright/tests/long-test-names.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '../src';
+
+test.describe('this is a very long story name it just keeps going and going and it cannot stop and it will not stop ba bada da da dum dum dum', () => {
+  test('and this is also an incredibly long test name because there are just a bunch of random chars at the end like this ldlk elke lekj felk felkf lkf lsf lkef lse flskef ls fls eflsj flksef', async ({
+    page,
+  }) => {
+    await page.goto('/');
+  });
+});

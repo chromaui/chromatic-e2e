4d1ffaf6c90215c313ddc331591b61a00001f5f9
Tom Coleman <tom@chromatic.com>
2024-07-10 16:55:26 +1000

    Add an extra export to the index

--- packages/playwright/src/index.ts
+++ packages/playwright/src/index.ts
@@ -6,4 +6,5 @@ export const test = makeTest(base);
 export { expect };
 
 export { takeSnapshot } from './takeSnapshot';
+export { createResourceArchive } from './createResourceArchive';
 export type { ChromaticConfig } from '@chromatic-com/shared-e2e';

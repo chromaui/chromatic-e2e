32a1e7cfa7ba91fa2ac309a9ae6794bb77c30e20
Andrew Ortwein <andrewortwein@icloud.com>
2024-09-03 15:42:30 -0400

    Fix test of protected page to expect to use global credentials

--- packages/playwright/tests/auth.spec.ts
+++ packages/playwright/tests/auth.spec.ts
@@ -1,19 +1,9 @@
 import { test, expect } from '../src';
 
-const httpCredentials = {
-  username: 'admin',
-  password: 'supersecret',
-};
-
 test.describe(() => {
-  test.use({
-    extraHTTPHeaders: {
-      Authorization: `Basic ${btoa(`${httpCredentials.username}:${httpCredentials.password}`)}`,
-    },
-  }),
-    test('can login', async ({ page }) => {
-      await page.goto('/im-protected');
+  test('can login', async ({ page }) => {
+    await page.goto('/auth');
 
-      await expect(page.getByText('I AM PROTECTED!!!')).toBeVisible();
-    });
+    await expect(page.getByText('I AM PROTECTED!!!')).toBeVisible();
+  });
 });

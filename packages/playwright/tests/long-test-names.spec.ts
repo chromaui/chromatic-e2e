57ef80cca9b2eca721264d8573f9689202efd5e4
Katie Bayes <katpoe@gmail.com>
2024-12-11 13:19:24 -0500

    add tests for long multibyte test/story names (#219)
    * add tests for long multibyte test/story names
    
    * Fix to truncate filename based on byte size
    
    * add changeset
    
    ---------
    
    Co-authored-by: Shohei Nakayama <nakker1218@gmail.com>

--- packages/playwright/tests/long-test-names.spec.ts
+++ packages/playwright/tests/long-test-names.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../src';
+import { test } from '../src';
 
 test.describe('this is a very long story name it just keeps going and going and it cannot stop and it will not stop ba bada da da dum dum dum', () => {
   test('and this is also an incredibly long test name because there are just a bunch of random chars at the end like this ldlk elke lekj felk felkf lkf lsf lkef lse flskef ls fls eflsj flksef', async ({
@@ -8,6 +8,12 @@ test.describe('this is a very long story name it just keeps going and going and 
   });
 
   test('and this is also an incredibly long test name because there are just a bunch of random chars at the end like this ldlk elke lekj felk felkf lkf lsf lkef lse flskef ls fls eflsj flksef 2', async ({
+    page,
+  }) => {
+    await page.goto('/');
+  });
+
+  test('multi-byte characters test case: ああだこうだ、ああだこうだ、ああだこうだ、ああだこうだ、ああだこうだ、ああだこうだ、ああだこうだ、ああだこうだ、ああだこうだ、ああだこうだ', async ({
     page,
   }) => {
     await page.goto('/');

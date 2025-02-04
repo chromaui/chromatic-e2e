6436ee1903378d229dc570f1db663e1686f31d2a
Steven Kitterman <steven.kitterman@gmail.com>
2025-02-04 13:45:46 -0800

    AMD tests cover blob URL case

--- test-server/fixtures/assets/scripts/amd.js
+++ test-server/fixtures/assets/scripts/amd.js
@@ -9,4 +9,13 @@ require(['lodash'], function (_) {
   const numbers = [1, 2, 3, 4, 5];
   const sum = _.sum(numbers);
   document.getElementById('output').textContent = `Sum of ${numbers.join('+')} = ${sum}`;
+
+  const inputElement = document.querySelector('input');
+  const imgElement = document.querySelector('#uploadImg');
+  inputElement.addEventListener('change', function () {
+    const url = URL.createObjectURL(inputElement.files[0]);
+    imgElement.src = url;
+    const urlText = document.querySelector('#objectUrl');
+    urlText.textContent = url;
+  });
 });

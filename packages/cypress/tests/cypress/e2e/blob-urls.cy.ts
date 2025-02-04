da4f2c27a2d187e4d116689902a4d5f13e1ef2e4
Steven Kitterman <steven.kitterman@gmail.com>
2025-02-04 14:10:44 -0800

    Test case (failing on purpose) for blob URLs in Cypress

---
+++ packages/cypress/tests/cypress/e2e/blob-urls.cy.ts
@@ -0,0 +1,45 @@
+// import * as path from 'path';
+// import { test, expect } from '../src';
+
+// test.use({ ignoreSelectors: ['#objectUrl'] });
+
+// test('Upload a Single file and Assert blob', async ({ page }) => {
+//   await page.goto('/createObjectUrl');
+//   const fileWithPath = path.join(__dirname, '../../../test-server/fixtures/blue.png');
+//   const [fileChooser] = await Promise.all([
+//     page.waitForEvent('filechooser'),
+//     page.locator('#fileInput').click(),
+//   ]);
+//   await fileChooser.setFiles([fileWithPath]);
+//   await page.locator('#fileInput').dispatchEvent('change');
+//   await expect(page.locator('#objectUrl')).toHaveText(/blob:.*/);
+// });
+
+// // adapted from https://fossies.org/linux/playwright/tests/library/trace-viewer.spec.ts
+// test('Fetch data for blob', async ({ page }) => {
+//   await page.goto('/createObjectUrl');
+//   await page.evaluate(async () => {
+//     const dataUrl =
+//       'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAASCAQAAADIvofAAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfhBhAPKSstM+EuAAAAvUlEQVQY05WQIW4CYRgF599gEZgeoAKBWIfCNSmVvQMe3wv0ChhIViKwtTQEAYJwhgpISBA0JSxNIdlB7LIGTJ/8kpeZ7wW5TcT9o/QNBtvOrrWMrtg0sSGOFeELbHlCDsQ+ukeYiHNFJPHBDRKlQKVEbFkLUT3AiAxI6VGCXsWXAoQLBUl5E7HjUFwiyI4zf/wWoB3CFnxX5IeGdY8IGU/iwE9jcZrLy4pnEat+FL4hf/cbqREKo/Cf6W5zASVMeh234UtGAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE3LTA2LTE2VDE1OjQxOjQzLTA3OjAwd1xNIQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxNy0wNi0xNlQxNTo0MTo0My0wNzowMAYB9Z0AAAAASUVORK5CYII=';
+//     const blob = await fetch(dataUrl).then((res) => res.blob());
+//     const url = window.URL.createObjectURL(blob);
+//     const img = document.createElement('img');
+//     img.id = 'blobImg';
+//     img.src = url;
+//     const loaded = new Promise((f) => (img.onload = f));
+//     document.body.appendChild(img);
+//     await loaded;
+//   });
+//   const size = await page.locator('#blobImg').evaluate((e) => (e as HTMLImageElement).naturalWidth);
+//   expect(size).toBe(10);
+// });
+
+it('assets from data urls are archived', () => {
+  cy.visit('/createObjectUrl');
+
+  cy.get('#fileInput').selectFile('../../../test-server/fixtures/blue.png');
+
+  cy.get('#objectUrl')
+    .invoke('text')
+    .should('match', /blob:.*/);
+});

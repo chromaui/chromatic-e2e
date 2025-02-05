6269fca3bc88d85ad1939168312a2f6848a063be
Steven Kitterman <steven.kitterman@gmail.com>
2025-02-05 10:53:13 -0800

    Comment on why file is ignored

--- packages/playwright/jest.config.js
+++ packages/playwright/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   testMatch: ['**/*.test.*'],
   testPathIgnorePatterns: ['__playwrightTests__/*'],
-  coveragePathIgnorePatterns: ['<rootDir>/src/inBrowserPostProcessing.js'],
+  // this file is ignored because code coverage was inserting some cov_asdf line that was making tests fail
+  // After trying to istanbul-ignore a single line, I ended up disabling the coverage completely for this file.
+  // https://stackoverflow.com/questions/55272295/using-jest-with-puppeteer-evaluation-failed-referenceerror-cov-4kq3tptqc-is
+  coveragePathIgnorePatterns: ['<rootDir>/src/inBrowserPostProcessing.ts'],
 };

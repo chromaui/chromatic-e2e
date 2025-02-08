78ff04ef0a5f5d033fd6dc390ab5d2e7f0597a98
Steven Kitterman <steven.kitterman@gmail.com>
2025-02-07 18:44:02 -0800

    Post-processing -- e.g. blob URL support -- is a templated string instead of function, as that was breaking when deployed

--- packages/playwright/src/takeSnapshot.ts
+++ packages/playwright/src/takeSnapshot.ts
@@ -3,7 +3,6 @@ import { readFileSync } from 'fs';
 import { dedent } from 'ts-dedent';
 import type { serializedNodeWithId } from '@chromaui/rrweb-snapshot';
 import { logger } from '@chromatic-com/shared-e2e';
-import { postProcessSnapshot } from './inBrowserPostProcessing';
 
 const rrweb = readFileSync(require.resolve('@chromaui/rrweb-snapshot'), 'utf8');
 
@@ -33,15 +32,72 @@ async function takeSnapshot(
     logger.log(`CONSOLE: "${msg.text()}"`);
   });
 
-  // page.evaluate can take in a string or a function
-  // below (when we post-process the DOM) we want to have that code as a function
-  // as it's easier to type, test, and (eventually) lint
-  // so we import the rrweb code into the same page separately, and then use it in the other
-  // page.evaluate call down below.
-  await page.evaluate(dedent`${rrweb}`);
-
   // Serialize and capture the DOM
-  const domSnapshot: serializedNodeWithId = await page.evaluate(postProcessSnapshot);
+  const domSnapshot: serializedNodeWithId = await page.evaluate(dedent`
+    ${rrweb};
+
+    // this code was erroring the page.evaluate() when it was passed as a function to page.evaluate(),
+    // so for now it is being passed as a string until that can be resolved.
+    const doPostProcessing = (rrwebSnapshotInstance, documentToSnapshot) => {
+      return new Promise((resolve) => {
+        const domSnapshot = rrwebSnapshotInstance.snapshot(documentToSnapshot);
+        // do some post-processing on the snapshot
+        const toDataURL = async (url) => {
+          // read contents of the blob URL
+          const response = await fetch(url);
+          const blob = await response.blob();
+          return new Promise((resolveFileRead, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolveFileRead(reader.result);
+            reader.onerror = reject;
+            // convert the blob to base64 string
+            reader.readAsDataURL(blob);
+          });
+        };
+
+        const replaceBlobUrls = async (node) => {
+          await Promise.all(
+            node.childNodes.map(async (childNode) => {
+              if (childNode.tagName === 'img' && childNode.attributes.src?.startsWith('blob:')) {
+                const base64Url = await toDataURL(childNode.attributes.src);
+                // eslint-disable-next-line no-param-reassign
+                childNode.attributes.src = base64Url;
+              }
+
+              if (childNode.childNodes?.length) {
+                await replaceBlobUrls(childNode);
+              }
+            })
+          );
+        };
+
+        replaceBlobUrls(domSnapshot).then(() => {
+          resolve(domSnapshot);
+        });
+      });
+  };
+
+  // page.evaluate returns the value of the function being evaluated. In this case, it means that
+  // it is returning either the resolved value of the Promise or the return value of the call to
+  // the snapshot function. See https://playwright.dev/docs/api/class-page#page-evaluate.
+  if (typeof define === 'function' && define.amd) {
+    // AMD support is detected, so we need to load rrwebSnapshot asynchronously
+    new Promise((resolve) => {
+      // eslint-disable-next-line import/no-dynamic-require, global-require
+      require(['rrwebSnapshot'], (rrwebSnapshot) => {
+        doPostProcessing(rrwebSnapshot, document).then((domSnapshot) => {
+          resolve(domSnapshot);
+        });
+      });
+    });
+  } else {
+    new Promise((resolve) => {
+      doPostProcessing(rrwebSnapshot, document).then((domSnapshot) => {
+        resolve(domSnapshot);
+      });    
+    });
+  }    
+  `);
 
   const bufferedSnapshot = Buffer.from(JSON.stringify(domSnapshot));
   if (!chromaticSnapshots.has(testId)) {

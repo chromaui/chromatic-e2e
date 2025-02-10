f09788e1d7afb4d1ea7758cf5d9f2225904bc1d7
Steven Kitterman <steven.kitterman@gmail.com>
2025-02-10 13:56:40 -0800

    AMD test fully fleshed out

--- packages/cypress/tests/cypress/e2e/amd.cy.ts
+++ packages/cypress/tests/cypress/e2e/amd.cy.ts
@@ -1,15 +1,11 @@
-it('pages with AMD modules are archived', () => {
+it('pages with AMD modules are archived', { env: { ignoreSelectors: ['#objectUrl'] } }, () => {
   cy.visit('/amd');
   cy.get('#output').contains('Sum of');
 
-  // // why are we testing blob URL stuff here? Because with AMD,
-  // // we need to call this blob stuff separately than non-AMD
-  // const fileWithPath = path.join(__dirname, '../../../test-server/fixtures/blue.png');
-  // const [fileChooser] = await Promise.all([
-  //   page.waitForEvent('filechooser'),
-  //   page.locator('#fileInput').click(),
-  // ]);
-  // await fileChooser.setFiles([fileWithPath]);
-  // await page.locator('#fileInput').dispatchEvent('change');
-  // await expect(page.locator('#objectUrl')).toHaveText(/blob:.*/);
+  cy.get('#fileInput').selectFile('../../../test-server/fixtures/blue.png');
+
+  cy.get('#objectUrl')
+    .invoke('text')
+    .should('match', /blob:.*/);
+  cy.takeSnapshot('Manual snapshot (Should show a blue square)');
 });

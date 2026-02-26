0aca75038a29c737e47a5f32c094bd30559be523
Norbert de Langen <ndelangen@me.com>
2026-02-26 14:50:13 +0100

    do not use addon, but add manager & previews directly

--- packages/shared/src/archive-storybook/index.ts
+++ packages/shared/src/archive-storybook/index.ts
@@ -7,10 +7,7 @@ import { addViewportsToStoriesFiles } from './viewports';
 
 // ESM: use import.meta.url. CJS: use __filename (Node provides it when running as CommonJS).
 declare const __filename: string;
-const moduleURL =
-  typeof require !== 'undefined'
-    ? pathToFileURL(__filename).href
-    : import.meta.url;
+const moduleURL = typeof require !== 'undefined' ? pathToFileURL(__filename).href : import.meta.url;
 const req = createRequire(moduleURL);
 
 export function archiveStorybook(
@@ -40,5 +37,5 @@ export function buildArchiveStorybook(
 function binPath() {
   const packageJson = req('storybook/package.json');
   // reference the entry file based on the package.json `bin` value, since it changed between SB 8.1 and 8.2
-  return resolve(dirname(req.resolve('storybook/package.json')), packageJson.bin.storybook);
+  return resolve(dirname(req.resolve('storybook/package.json')), packageJson.bin);
 }

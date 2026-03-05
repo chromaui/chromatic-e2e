ea2ae049f6ee5bdd83ed5f205f355673f9173f1f
Norbert de Langen <ndelangen@me.com>
2026-03-05 11:14:28 +0100

    remove minification & sourcemap generation

--- packages/playwright/tsup.config.ts
+++ packages/playwright/tsup.config.ts
@@ -1,23 +1,19 @@
 import { Options, defineConfig } from 'tsup';
 
-const common = (options) => ({
-  minify: !options.watch,
+const common: Partial<Options> = {
   splitting: false,
   dts: {
     resolve: true,
   },
   treeshake: true,
-  sourcemap: true,
+  minify: false,
   clean: false, // This set to `true` caused a race condition since we're running multiple builds below
-  esbuildOptions(options) {
-    options.conditions = ['module'];
-  },
-});
+};
 
 export default defineConfig((options) => [
   // We want the playwright functions to be importable from both CJS or ESM files
   {
-    ...common(options),
+    ...common,
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
     platform: 'node',
@@ -29,11 +25,11 @@ export default defineConfig((options) => [
         js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);",
       };
     },
-  } as Options,
+  },
   // These are all node scripts so we keep it simple and only generate CJS.
   // In particular SB will warn if we generate both CJS+ESM main files
   {
-    ...common(options),
+    ...common,
     entry: {
       'bin/archive-storybook': 'src/bin/archive-storybook.ts',
       'bin/build-archive-storybook': 'src/bin/build-archive-storybook.ts',
@@ -42,10 +38,13 @@ export default defineConfig((options) => [
     },
     format: ['cjs'],
     platform: 'node',
-  } as Options,
+    esbuildOptions(options) {
+      options.conditions = ['module'];
+    },
+  },
   // This is a SB browser file so ESM is better.
   {
-    ...common(options),
+    ...common,
     entry: {
       'storybook-config/preview': 'src/storybook-config/shared/preview.ts',
     },
@@ -53,6 +52,8 @@ export default defineConfig((options) => [
     platform: 'browser',
     // We need to be careful how we minimize `preview.ts` because the
     // SB indexer is quite particular about the format of `parameters`.
-    minify: false,
-  } as Options,
+    esbuildOptions(options) {
+      options.conditions = ['module'];
+    },
+  },
 ]);

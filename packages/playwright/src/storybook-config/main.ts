8b1d6027d347d939384790246a0ab9808c5c95b5
Norbert de Langen <ndelangen@me.com>
2026-03-09 12:52:51 +0100

    add ESM-CJS compatibility code & manually set builder to full path

--- packages/playwright/src/storybook-config/main.ts
+++ packages/playwright/src/storybook-config/main.ts
@@ -1,6 +1,14 @@
-import path from 'path';
+import path from 'node:path';
+import { createRequire } from 'node:module';
+import { fileURLToPath } from 'node:url';
+
 import { archivesDir } from '@chromatic-com/shared-e2e/utils/filePaths';
+
 import { DEFAULT_OUTPUT_DIR } from '../constants';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const require = createRequire(import.meta.url);
 
 function getAbsolutePath(value: string) {
   return path.dirname(require.resolve(path.join(value, 'package.json')));
@@ -14,6 +22,9 @@ export default {
   framework: {
     name: getAbsolutePath('@storybook/server-webpack5'),
     options: {},
+  },
+  core: {
+    builder: import.meta.resolve('@storybook/server-webpack5'),
   },
   staticDirs: [path.resolve(archivesDir(DEFAULT_OUTPUT_DIR), 'archive')],
 };

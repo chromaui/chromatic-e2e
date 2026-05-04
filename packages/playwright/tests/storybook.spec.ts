33fdb89c23fec09a84c8d90b263581d07e3b3a7d
Ari Perkkiö <ari.perkkio@gmail.com>
2026-05-04 10:51:06 +0300

    test: update published storybook URLs

--- packages/playwright/tests/storybook.spec.ts
+++ packages/playwright/tests/storybook.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '../src';
 test.use({ assetDomains: ['localhost:6006'] });
 test('visits hosted storybook page', async ({ page }) => {
   await page.goto(
-    'https://main--653fef099b8957739e7534a4.chromatic.com/iframe.html?globals=viewport:w1280h720&id=options-pause-animation-at-end--snapshot-1&viewMode=story'
+    'https://main--653fef099b8957739e7534a4.chromatic.com/iframe.html?id=options-pause-animation-at-end--snapshot-1&viewMode=story'
   );
   await expect(page.getByRole('heading', { name: /The image/i })).toBeVisible();
 });

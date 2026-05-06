76d1de64b44b9a95e8c5bdc929537f9a2927ebe5
Steven Kitterman <steven.kitterman@gmail.com>
2026-05-06 16:18:16 -0700

    Cypress embed test updated to work

--- packages/cypress/tests/cypress/e2e/embeds.cy.ts
+++ packages/cypress/tests/cypress/e2e/embeds.cy.ts
@@ -12,34 +12,44 @@ it('cross-origin embed page loads', () => {
 });
 
 it('embedded page background color can be changed', { env: { disableAutoSnapshot: true } }, () => {
+  /** Re-query each time; wait until iframe document + body exist (load can lag after parent paint). */
+  const inEmbeddedBody = (fn: () => void) => {
+    cy.get('iframe[title="Same-origin iframe"]')
+      .should(($iframe) => {
+        const el = $iframe[0] as HTMLIFrameElement;
+        const body = el.contentDocument?.body;
+        expect(body, 'iframe body').to.not.equal(null);
+        expect(body!.querySelector('h1')?.textContent).to.include('Embedded page');
+      })
+      .then(($iframe) => {
+        const body = ($iframe[0] as HTMLIFrameElement).contentDocument!.body;
+        cy.wrap(body).within(fn);
+      });
+  };
+
   cy.visit('/embeds/same-origin');
   cy.contains('h1', 'Embeds').should('be.visible');
 
-  cy.get('iframe[title="Same-origin iframe"]')
-    .its('0.contentDocument.body')
-    .should('not.be.empty')
-    .as('embeddedBody');
-
-  cy.get('@embeddedBody').within(() => {
+  inEmbeddedBody(() => {
     cy.contains('h1', 'Embedded page').should('be.visible');
     cy.contains('button', 'Red').click();
     cy.root().should('have.css', 'background-color', 'rgb(255, 0, 0)');
   });
   cy.takeSnapshot('Red background');
 
-  cy.get('@embeddedBody').within(() => {
+  inEmbeddedBody(() => {
     cy.contains('button', 'Yellow').click();
     cy.root().should('have.css', 'background-color', 'rgb(255, 255, 0)');
   });
   cy.takeSnapshot('Yellow background');
 
-  cy.get('@embeddedBody').within(() => {
+  inEmbeddedBody(() => {
     cy.contains('button', 'Blue').click();
     cy.root().should('have.css', 'background-color', 'rgb(0, 0, 255)');
   });
   cy.takeSnapshot('Blue background');
 
-  cy.get('@embeddedBody').within(() => {
+  inEmbeddedBody(() => {
     cy.contains('button', 'Reset').click();
     cy.root().should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
   });

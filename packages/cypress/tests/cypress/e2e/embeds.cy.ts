06a8edbef4fd59b011a07a5029a84b8eed73d572
Steven Kitterman <steven.kitterman@gmail.com>
2026-05-06 16:00:23 -0700

    Cypress test updated

--- packages/cypress/tests/cypress/e2e/embeds.cy.ts
+++ packages/cypress/tests/cypress/e2e/embeds.cy.ts
@@ -11,23 +11,37 @@ it('cross-origin embed page loads', () => {
   cy.request('http://localhost:3001/').its('body').should('include', 'Embedded page');
 });
 
-it('embedded page background color can be changed', () => {
-  cy.visit('/embeds/embedded-page');
-  cy.contains('h1', 'Embedded page').should('be.visible');
+it('embedded page background color can be changed', { env: { disableAutoSnapshot: true } }, () => {
+  cy.visit('/embeds/same-origin');
+  cy.contains('h1', 'Embeds').should('be.visible');
 
-  cy.contains('button', 'Red').click();
-  cy.get('body').should('have.css', 'background-color', 'rgb(255, 0, 0)');
+  cy.get('iframe[title="Same-origin iframe"]')
+    .its('0.contentDocument.body')
+    .should('not.be.empty')
+    .as('embeddedBody');
+
+  cy.get('@embeddedBody').within(() => {
+    cy.contains('h1', 'Embedded page').should('be.visible');
+    cy.contains('button', 'Red').click();
+    cy.root().should('have.css', 'background-color', 'rgb(255, 0, 0)');
+  });
   cy.takeSnapshot('Red background');
 
-  cy.contains('button', 'Yellow').click();
-  cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 0)');
+  cy.get('@embeddedBody').within(() => {
+    cy.contains('button', 'Yellow').click();
+    cy.root().should('have.css', 'background-color', 'rgb(255, 255, 0)');
+  });
   cy.takeSnapshot('Yellow background');
 
-  cy.contains('button', 'Blue').click();
-  cy.get('body').should('have.css', 'background-color', 'rgb(0, 0, 255)');
+  cy.get('@embeddedBody').within(() => {
+    cy.contains('button', 'Blue').click();
+    cy.root().should('have.css', 'background-color', 'rgb(0, 0, 255)');
+  });
   cy.takeSnapshot('Blue background');
 
-  cy.contains('button', 'Reset').click();
-  cy.get('body').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+  cy.get('@embeddedBody').within(() => {
+    cy.contains('button', 'Reset').click();
+    cy.root().should('have.css', 'background-color', 'rgba(0, 0, 0, 0)');
+  });
   cy.takeSnapshot('Reset background');
 });

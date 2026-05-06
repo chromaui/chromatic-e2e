0e5e754fed95d8e25accc825c047c2dbf5ff5790
Steven Kitterman <steven.kitterman@gmail.com>
2026-05-06 15:07:46 -0700

    Test for cross-origin iframe

--- packages/playwright/tests/embeds.spec.ts
+++ packages/playwright/tests/embeds.spec.ts
@@ -5,3 +5,14 @@ test('same-origin embed page loads', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Embeds' })).toBeVisible();
   await expect(page.locator('iframe[title="Same-origin iframe"]')).toBeVisible();
 });
+
+test('cross-origin embed page loads', async ({ page }) => {
+  await page.goto('/embeds/cross-origin');
+  await expect(page.getByRole('heading', { name: 'Embeds' })).toBeVisible();
+  await expect(page.locator('iframe[title="Cross-origin iframe"]')).toBeVisible();
+  await expect(
+    page
+      .frameLocator('iframe[title="Cross-origin iframe"]')
+      .getByText('Testing testing just a basic page')
+  ).toBeVisible();
+});

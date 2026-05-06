7c21ca36442a563d50afbb1cd4bd68dcfb9715f8
Steven Kitterman <steven.kitterman@gmail.com>
2026-05-06 15:08:00 -0700

    Some Vite config I honestly don't understand

--- packages/vitest/test/utils/global-setup.ts
+++ packages/vitest/test/utils/global-setup.ts
@@ -1,8 +1,9 @@
 export default async function setup() {
   // @ts-expect-error -- untyped
-  const { server } = await import('../../../../test-server/server');
+  const { server, embedServer } = await import('../../../../test-server/server');
 
   return function teardown() {
     server.close();
+    embedServer.close();
   };
 }

84b67add2df3338d251f56b1109efacba51da442
Ari Perkkiö <ari.perkkio@gmail.com>
2026-09-25 10:42:26 +0300

    fix(playwright): restore support for `projects[].use.viewport` (#444)

--- packages/playwright/src/bin/archive-storybook.ts
+++ packages/playwright/src/bin/archive-storybook.ts
@@ -5,12 +5,17 @@ import path from "node:path";
 import { archiveStorybook } from "@chromatic-com/shared-e2e/archive-storybook";
 
 import { DEFAULT_OUTPUT_DIR } from "../constants";
+import { addViewportsToStoriesFiles } from "./set-viewports";
 
 // Discard first two entries (exec path and file path)
 const args = process.argv.slice(2);
 const configDir = path.resolve(import.meta.dirname, "../storybook-config");
 
 try {
+  // When Playright projects are used, they override each others *.stories.json files. Re-apply viewports based on generated archives.
+  // Proper fix would be to prefix each *.stories.json file with the project name (like Vitest integration does), but that's breaking change.
+  await addViewportsToStoriesFiles();
+
   await archiveStorybook(args, configDir, DEFAULT_OUTPUT_DIR);
 } catch (err) {
   // Throwing the error results in a large output of minified code and a stacktrace that is
